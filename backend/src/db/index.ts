128b9ee7f2985bcac4a0672f2f39867f8c2a3624
chirantan-2025 <chirantanbramhachari560@gmail.com>
2026-02-19 21:11:30 +0530

    batabase created

---
+++ backend/src/db/index.ts
@@ -0,0 +1,27 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
+import { ENV } from "../config/env";
+
+if (!ENV.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in environment variables");
+}
+
+const pool = new Pool({ connectionString: ENV.DATABASE_URL });
+
+pool.on("connect", () => {
+  console.log("Database connected successfully");
+});
+
+pool.on("error", (err) => {
+  console.log("Database connection error:", err);
+});
+
+export const db = drizzle({ client: pool, schema });
+
+// What is a Connection pool?
+// A connection pool is a cache of database connections that are kept open and reused.
+
+//Why use it?
+//Opening/Closing Connection is slow. Instead of creating a new connection for each request, we reuse existing ones.
+//Database limit concurrent connections. A pool manages a fixed number of connections and shares then across requests
--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -2,12 +2,16 @@ import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
 export const users = pgTable("users", {
-  id: text("id").primaryKey(), // clearId
+  id: text("id").primaryKey(), // clerkId
   email: text("email").notNull().unique(),
   name: text("name"),
   imageUrl: text("image_url"),
   createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
-  updateAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow(),
+  // updatedAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", { mode: "date" })
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
 });
 
 export const products = pgTable("products", {
@@ -19,7 +23,7 @@ export const products = pgTable("products", {
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
   createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
-  updateAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow(),
 });
 
 export const comments = pgTable("comments", {
@@ -28,34 +32,47 @@ export const comments = pgTable("comments", {
   userId: text("user_id")
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
-  productId: text("product_id")
+  productId: uuid("product_id")
     .notNull()
     .references(() => products.id, { onDelete: "cascade" }),
   createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
-  updateAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow(),
 });
 
-//Relations define how tables connect to each other. This enables Drizzle's query API to automatically join related data when using 'with:{relationName:true}'
+// 🔴 Relations define how tables connect to each other. This enables Drizzle's query API
+// 🔴 to automatically join related data when using `with: { relationName: true }`
 
-//User can have many products and many comments
-export const userRelations = relations(users, ({ many }) => ({
-  product: many(products), //one user -> many products
-  comment: many(comments), //one user -> many products
+// 🔴 Users Relations: A user can have many products and many comments
+// 🔴 `many()` means one user can have multiple related records
+
+export const usersRelations = relations(users, ({ many }) => ({
+  products: many(products), // 🔴 One user → many products
+  comments: many(comments), // 🔴 One user → many comments
 }));
 
-//products belong to one user and can have many many comments
-export const productRelations = relations(products, ({ one, many }) => ({
-  //'fields' = the foreign key column in THIS table (product.userId)
-  //'references'=the primary key column in the RELATED table (users.id)
-  user: one(users, { fields: [products.userId], references: [users.id] }),
-  comment: many(comments),
+// Products Relations: a product belongs to one user and can have many comments
+// `one()` means a single related record, `many()` means multiple related records
+
+export const productsRelations = relations(products, ({ one, many }) => ({
+  comments: many(comments),
+  // `fields` = the foreign key column in THIS table (products.userId)
+  // `references` = the primary key column in the RELATED table (users.id)
+  user: one(users, { fields: [products.userId], references: [users.id] }), // one product → one user
 }));
 
-//comment belongs to one product and one user
-export const commentRelations = relations(comments, ({ one }) => ({
-  user: one(users, { fields: [comments.userId], references: [users.id] }),
-  product: one(products, {
-    fields: [comments.productId],
-    references: [products.id],
-  }),
+// Comments Relations: A comment belongs to one user and one product
+export const commentsRelations = relations(comments, ({ one }) => ({
+  // `comments.userId` is the foreign key,  `users.id` is the primary key
+  user: one(users, { fields: [comments.userId], references: [users.id] }), // One comment → one user
+  // `comments.productId` is the foreign key,  `products.id` is the primary key
+  product: one(products, { fields: [comments.productId], references: [products.id] }), // One comment → one product
 }));
+
+// Type inference
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
+
+export type Product = typeof products.$inferSelect;
+export type NewProduct = typeof products.$inferInsert;
+
+export type Comment = typeof comments.$inferSelect;
+export type NewComment = typeof comments.$inferInsert;
